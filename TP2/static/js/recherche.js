90ed326ea23d1b669ae2b4759c8386ad0840ba2c
TOUQUI <julienbeaumont@hotmail.com>
2023-12-03 16:45:14 -0500

    Bar de recherche fonctionel

--- TP2/static/js/recherche.js
+++ TP2/static/js/recherche.js
@@ -2,6 +2,12 @@
 
 let controleur = null;
 
+
+
+var la_div = document.getElementById("divDesRecherches");
+
+
+
 async function ajout_info_recherche() {
     var input = document.getElementById("contenu").value
     input.trim()
@@ -12,38 +18,39 @@ async function ajout_info_recherche() {
 
     controleur = new AbortController();
 
-    if (input.length > 2) {
-        var resultats = envoyerRequeteAjax('/api/recherche', "GET", parametres, controleur);
-        await resultats
-        resultats = resultats.then()
-        console.log(resultats);
-        var la_div = document.getElementById("divDesRecherches");
-        console.log("etape 1");
+    vider_la_div()
 
-        for (var item in resultats){
-            console.log(item.id)
-        }
+    if (input.length > 2) {
+        var resultats = await envoyerRequeteAjax('/api/recherche', "GET", parametres, controleur);
 
         for (var i = 0; i < resultats.length; i++) {
-            console.log("Dans boucle")
             var nouveau_resultat = document.createElement("a");
 
             nouveau_resultat.href = "/objet/troqueur/" + resultats[i].id;
             nouveau_resultat.className = "bg-light p-2 text-decoration-none text-black px-5 border-3 border-dark rounded";
+            nouveau_resultat.id = "propo" + i;
             var nom = document.createTextNode(resultats[i].titre);
             nouveau_resultat.appendChild(nom);
             la_div.appendChild(nouveau_resultat);
-            console.log("ajouter");
         }
     }
 
 }
 
 
+async function vider_la_div(){
+        la_div.innerHTML = "";
+}
+
+
+
 function initialisation() {
     document
         .getElementById("contenu")
         .addEventListener("input", ajout_info_recherche);
+    document
+        .getElementById("contenu")
+        .addEventListener("focusout", vider_la_div);
 }
 
 window.addEventListener("load", initialisation);
